e15e6981aa34acfe9d94c2a778fa94d7a755a225
narspi <ivanovmichurina@gmail.com>
2025-05-09 16:48:05 +0300

    index page finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,59 +1,69 @@
 import Swiper from "swiper";
-import { Navigation } from 'swiper/modules';
+import { Navigation } from "swiper/modules";
 
-document.addEventListener('DOMContentLoaded',()=>{
-    const services = document.querySelector('.services');
-    const certificatesSlider = document.querySelector('.certificates__slider');
-    const reviewsSlider = document.querySelector('.reviews__slider');
-    const questionsItems = document.querySelector('.questions__items');
+document.addEventListener("DOMContentLoaded", () => {
+  const services = document.querySelector(".services");
+  const certificatesSlider = document.querySelector(".certificates__slider");
+  const reviewsSlider = document.querySelector(".reviews__slider");
+  const questionsItems = document.querySelector(".questions__items");
+  const footerBtnScrollUp = document.querySelector(".footer__scroll-up");
 
-    if (services) {
-        services.addEventListener('click', (event) => {
-            const target = event.target;
-            if (target.closest('.services__elem-title')) {
-                const titleElem = target.closest('.services__elem-title');
-                const parent = target.closest('.services__elem')
-                const elemDropdown = parent.querySelector('.services__elem-drop');
-                elemDropdown.classList.toggle('active');
-                titleElem.classList.toggle('active');
-            }
-        })
-    }
+  if (services) {
+    services.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.closest(".services__elem-title")) {
+        const titleElem = target.closest(".services__elem-title");
+        const parent = target.closest(".services__elem");
+        const elemDropdown = parent.querySelector(".services__elem-drop");
+        elemDropdown.classList.toggle("active");
+        titleElem.classList.toggle("active");
+      }
+    });
+  }
 
-    if (certificatesSlider) {
-        new Swiper(certificatesSlider, {
-            modules: [Navigation],
-            slidesPerView: 'auto',
-            spaceBetween: 24,
-            navigation: {
-                nextEl: '.certificates__slider-next',
-                prevEl: '.certificates__slider-prev',
-            },
-        })
-    }
+  if (certificatesSlider) {
+    new Swiper(certificatesSlider, {
+      modules: [Navigation],
+      slidesPerView: "auto",
+      spaceBetween: 24,
+      navigation: {
+        nextEl: ".certificates__slider-next",
+        prevEl: ".certificates__slider-prev",
+      },
+    });
+  }
 
-    if (reviewsSlider) {
-        new Swiper(reviewsSlider, {
-            modules: [Navigation],
-            slidesPerView: 'auto',
-            spaceBetween: 24,
-            navigation: {
-                nextEl: '.reviews__btn-next',
-                prevEl: '.reviews__btn-prev',
-            },
-        })
-    }
+  if (reviewsSlider) {
+    new Swiper(reviewsSlider, {
+      modules: [Navigation],
+      slidesPerView: "auto",
+      spaceBetween: 24,
+      navigation: {
+        nextEl: ".reviews__btn-next",
+        prevEl: ".reviews__btn-prev",
+      },
+    });
+  }
 
-    if (questionsItems) {
-        questionsItems.addEventListener('click', (event) => {
-            const target = event.target;
-            if (target.closest('.questions__item-title')) {
-                const parent = target.closest('.questions__item')
-                const btnElem = parent.querySelector('.questions__item-btn');
-                const elemDropdown = parent.querySelector('.questions__item-drop');
-                elemDropdown.classList.toggle('active');
-                btnElem.classList.toggle('active');
-            }
-        })
-    }
+  if (questionsItems) {
+    questionsItems.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.closest(".questions__item-title")) {
+        const parent = target.closest(".questions__item");
+        const btnElem = parent.querySelector(".questions__item-btn");
+        const elemDropdown = parent.querySelector(".questions__item-drop");
+        elemDropdown.classList.toggle("active");
+        btnElem.classList.toggle("active");
+      }
+    });
+  }
+
+  if (footerBtnScrollUp) {
+    footerBtnScrollUp.addEventListener("click", function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+  }
 });
